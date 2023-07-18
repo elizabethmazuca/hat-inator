ed989c7403326b64c91f297c7376dd236101cb4d
elizabethmazuca <elizabethmazuca@csu.fullerton.edu>
2023-07-18 00:22:18 -0700

    everything but the key

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
-const API_KEY = //key (do not post bozo)
+const API_KEY = "" //key (do not post bozo)
 
 const submitIcon = document.querySelector("#submit-icon") //like an event listener
 const inputElement = document.querySelector("input") //grabs user input
 
-const getImages = () => {
+const getImages = async () => { //asynch bc we are using await
     const options = {
-        mathod: "POST",
+        method: "POST",
         headers: {
-            "Authorization": 'Bearer ${API_KEY}',
+            "Authorization": `Bearer ${API_KEY}`,
             'Content-Type': "application/json"
         },
         body: JSON.stringify({
@@ -19,8 +19,12 @@ const getImages = () => {
     }
 
     try {
-        fetch('https://api.openai.com/v1/images/generations', options)
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        console.log(data)
     } catch (error) {
         console.error(error)
     }
 }
+
+submitIcon.addEventListener('click', getImages)
